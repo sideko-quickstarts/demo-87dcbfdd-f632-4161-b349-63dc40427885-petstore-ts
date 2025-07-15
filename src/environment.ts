681b70d48526df0cebef709cc1dff81ccee0fdf6
Elias Posen <elias+local@sideko.dev>
2025-07-15 18:14:22 -0400

    Initial commit: Typescript SDK for demo

---
+++ src/environment.ts
@@ -0,0 +1,4 @@
+export enum Environment {
+  Environment = "https://petstore3.swagger.io/api/v3",
+  MockServer = "http://127.0.0.1:8082/v1/mock/demo/demo-api-40/0.1.0",
+}
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { Client as default } from "demo_api_40_ts/client";
+export {
+  ApiError,
+  ApiResponse,
+  BinaryResponse,
+  RequestOptions,
+  UploadFile,
+  isUploadFile,
+} from "demo_api_40_ts/core";
+export { Environment } from "demo_api_40_ts/environment";
+export * as resources from "demo_api_40_ts/resources";
+export * as types from "demo_api_40_ts/types";
